607321f9ff6c8b10beaee88c433f3edbc92b03a1
seanmars <mars0222@gmail.com>
2025-09-02 00:31:28 +0800

    Refactor crawler function to use Playwright's webkit and improve page navigation

--- ithomeIrom.py
+++ ithomeIrom.py
@@ -2,17 +2,31 @@ import asyncio
 from playwright.async_api import async_playwright, Playwright
 
 
-async def crawler(page: int):
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto(f"https://ithelp.ithome.com.tw/2025ironman?page={page}")
-        
-        # Add your scraping logic here
-        # get xpath
-        pages = await page.locator("//*[@id='ir-list']/div/nav/div/span[3]").inner_text()
-        print(pages)
+async def crawler(playwright: Playwright, pageNumber: int):
+    webkit = playwright.webkit
+    browser = await webkit.launch(headless=True)
+    context = await browser.new_context()
+    page = await context.new_page()
+    try:
+        url = f"https://ithelp.ithome.com.tw/2025ironman?page={pageNumber}"
+        print(f"goto page {url}")
+        response = await page.goto(url)
+        print(f"response status: {response.status}")
+        if response.status != 200:
+            print(f"Failed to load page {pageNumber}")
+            await browser.close()
+            return
+
+        print(f"scraping page {pageNumber}")
+        selector = "#ir-list > div > section > section.sec-articles > div.container > div.row:nth-child(2) div.articles-box"
+        articleBoxs = await page.locator(selector).all()
+    finally:
         await browser.close()
 
+
+async def main():
+    async with async_playwright() as playwright:
+        await crawler(playwright, 1)
+
 if __name__ == "__main__":
-    asyncio.run(crawler(1))
+    asyncio.run(main())
